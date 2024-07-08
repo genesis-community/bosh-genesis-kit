46efabc79b9056657e56c9484ea6ba1041798160
Dennis J. Bell <dennis.j.bell@gmail.com>
2024-07-08 11:59:04 -0700

    Adds perl-based hooks for addon-runtime-config and blueprint
    [Improvements]
    
    * This commit adds two new perl-based hooks for the addon-runtime-config and blueprint commands. These hooks are based on the existing bash-based hooks, but have been rewritten in perl to take advantage of the Genesis perl library for speed and existing library routines.
    
      Specifically, the addon-runtime-config hook has been updated to take advantage of entombification of secrets, so that deployments don't leak secrets. The blueprint hook has been written in perl to speed up the execution of the blueprint command.

---
+++ hooks/addon-runtime-config~rc.pm
@@ -0,0 +1,456 @@
+#!/usr/bin/env perl
+package Genesis::Hook::Addon::BOSH::RuntimeConfig v3.0.4;
+
+use strict;
+use warnings;
+
+# Only needed for development
+my $lib;
+BEGIN {$lib = $ENV{GENESIS_LIB} ? $ENV{GENESIS_LIB} : $ENV{HOME}.'/.genesis/lib'}
+use lib $lib;
+
+use parent qw(Genesis::Hook::Addon);
+
+use Genesis qw/bail info warning error in_array new_enough time_exec mkfile_or_fail pretty_duration run/;
+use Genesis::Term qw/terminal_width render_markdown decolorize/;
+use JSON::PP;
+
+sub init {
+	my $class = shift;
+	my $obj = $class->SUPER::init(@_);
+	$obj->check_minimum_genesis_version('3.0.0-rc.1');
+	return $obj;
+}
+
+sub cmd_details {
+	return 
+		"Generate the runtime config, and upload it to the target BOSH director\n".
+		"\n".
+		"Options:\n".
+		"[[  #y{-n}      >>Dry run, just print out the runtime config without ".
+		                  "uploading it.\n".
+		"[[  #y{-y}      >>Upload changes without prompting for confirmation.\n".
+		"[[  #y{-R}      >>Remove the runtime config from the director instead.\n".
+		"[[  #y{-d}      >>Upload the config to the default runtime config, ".
+		                  "merging with what is currently there.  This is not ".
+		                  "recommended, but included for backwards compatibility"
+}
+
+sub bosh { # bosh needs to point to itself, not its parent
+	my $self = shift;
+	return $self->{__bosh} ||= sub {
+		my $self = shift;
+		my $exodus_data = $self->exodus_data();
+		my $bosh;
+		if ($exodus_data->{url} && $exodus_data->{admin_password}) {
+			$bosh = Service::BOSH::Director->from_exodus($self->env->name, exodus_data => $exodus_data);
+		} else {
+			$bosh = Service::BOSH::Director->from_alias($self->env->name);
+		}
+		$bosh->connect_and_validate();
+		$bosh;
+	}->($self);
+}
+
+sub credhub { # Like bosh, credhub needs to point to itself, not its parent
+	my $self = shift;
+	return $self->{__credhub} ||= sub {
+		my ($self) = @_;
+		require Service::Credhub;
+		my $exodus = $self->exodus_data();
+		my $credhub = Service::Credhub->new(
+			$self->env->deployment_name,
+			"/", # No root path
+			$exodus->{credhub_url},
+			$exodus->{credhub_username},
+			$exodus->{credhub_password},
+			sprintf("%s%s",$exodus->{ca_cert}||"",$exodus->{credhub_ca_cert}||"")
+		);
+		return $credhub;
+	}->($self);
+}
+
+sub _get_secret {
+	my ($self, $secret) = @_;
+	my ($path, $key) = split(/:/, $secret, 2);
+	my $base_path = $self->env->secrets_store->base;
+	$self->vault->get($base_path.$path,$key);
+}
+
+sub _upload_runtime_config {
+	my ($self, $name, $contents) = @_;
+	my $file = mkfile_or_fail($self->env->workpath('runtime-configs.yml'), $contents);
+	my ($out, $rc, $err) = $self->bosh->execute({interactive => 1},
+		'update-runtime-config',
+		'--tty',
+		'--no-redact',
+		'--name='.$name,
+		$file
+	);
+	$out = decolorize($out) =~ s/\r//rg; # remove color codes and CRs
+	$err = $err 
+		? $err 
+	: ($rc == 0 && $out =~ /^Succeeded$/m)
+		? ""
+	: ($out =~ /^Stopped$/m)
+		? "Operation stopped by user"
+		: "Unknown error";
+
+	if ($rc) {
+		if ($err eq "Operation stopped by user") {
+			warning("\nDid not upload runtime config: %s\n", $err);
+		} else {
+			bail("Failed to upload runtime config: %s", $err);
+		}
+	}
+	return $out;
+}
+
+sub _get_runtime_config {
+	my ($self, $name) = @_;
+	my $config = $self->read_json_from_bosh('config', '--type=runtime', '--name='.$name);
+	return $config ? $config->[0]{content} : "";
+}
+
+sub _remove_runtime_config {
+	my ($self, $name) = @_;
+	my $config = $self->_get_runtime_configs($name);
+	if ($config) {
+		info("Removing existing '#C{%s}' runtime:", $name);
+		info("  - $_") for split(/\n/, $config);
+		info("");
+		$self->bosh->execute({interactive => 1}, 'delete-config', '--type=runtime', '--name='.$name);
+	}
+}
+
+sub _get_runtime_configs {
+	$_[0]->read_json_from_bosh('configs', '--type=runtime');
+}
+
+sub _generate_dns_runtime {
+	my $self = shift;
+
+	my $dns_runtime = {
+		addons => [
+			{
+				name => 'bosh-dns',
+				include => {
+					stemcell => [
+						{os => 'ubuntu-xenial'},
+						{os => 'ubuntu-bionic'},
+						{os => 'ubuntu-jammy'} ] },
+				jobs => [
+					{
+						name => 'bosh-dns',
+						release => 'bosh-dns',
+						properties => {
+							api => {
+								client => {
+									tls => {
+										ca => $self->_get_secret('dns_api_tls/ca:certificate'),
+										certificate => $self->_get_secret('dns_api_tls/client:certificate'),
+										private_key => $self->_get_secret('dns_api_tls/client:key') } },
+								server => {
+									tls => {
+										ca => $self->_get_secret('dns_api_tls/ca:certificate'),
+										certificate => $self->_get_secret('dns_api_tls/server:certificate'),
+										private_key => $self->_get_secret('dns_api_tls/server:key') } }
+							},
+							cache => {
+								enabled => scalar($self->env->lookup('dns_cache', JSON::PP::true))
+							} } } ] } ]
+	};
+	my $whitelist = $self->env->lookup('dns_deployments_whitelist', []);
+	if (@$whitelist) {
+		push @{$dns_runtime->{addons}[0]{include}{deployments}}, map { {name => $_} } @$whitelist;
+	}
+	my $blacklist = $self->env->lookup('dns_deployments_blacklist', []);
+	my $ig_blacklist = $self->env->lookup('dns_instance_groups_blacklist', []);
+	if (@$blacklist || @$ig_blacklist) {
+		$dns_runtime->{addons}[0]{exclude} = {};
+		$dns_runtime->{addons}[0]{exclude}{deployments} = [map { {name => $_} } @$blacklist] if @$blacklist;
+		$dns_runtime->{addons}[0]{exclude}{instance_groups} = [map { {name => $_} } @$ig_blacklist] if @$ig_blacklist;
+	}
+	if ($self->want_feature('bosh-dns-healthcheck')) {
+		$dns_runtime->{addons}[0]{jobs}[0]{properties}{health} = {
+			enabled => JSON::PP::true,
+			client => {
+				tls => {
+					ca => $self->_get_secret('dns_healthcheck_tls/ca:certificate'),
+					certificate => $self->_get_secret('dns_healthcheck_tls/client:certificate'),
+					private_key => $self->_get_secret('dns_healthcheck_tls/client:key') } },
+			server => {
+				tls => {
+					ca => $self->_get_secret('dns_healthcheck_tls/ca:certificate'),
+					certificate => $self->_get_secret('dns_healthcheck_tls/server:certificate'),
+					private_key => $self->_get_secret('dns_healthcheck_tls/server:key') } }
+		};
+	}
+	my $upstream_release = scalar($self->spruce_merge(
+		'--skip-eval',
+		'--cherry-pick',
+		'releases',
+		$self->kit->path('bosh-deployment/runtime-configs/dns.yml')
+	));
+
+	my ($out, $rc, $err) = run(
+		'spruce merge <(echo "$1") <(echo "$2") $3',
+		JSON::PP::encode_json($dns_runtime),
+		$upstream_release,
+		$self->kit->path('overlay/releases/bosh-dns.yml')
+	);
+	bail("Failed to merge DNS runtime: %s", $err) if $rc;
+	return $out;
+}
+
+sub _generate_ops_access_runtime {
+	my $self = shift;
+	return "" unless grep { $_ =~ /^(net|sys)op-access$/ } $self->features;
+
+	my $ops_access_runtime = {
+		addons => [
+			{
+				name => 'genesis-local-users',
+				exclude => {
+					jobs => [
+						{name => 'user_add', release => 'os-conf'}
+					]
+				},
+				jobs => [
+					{
+						name => 'user_add',
+						release => 'os-conf',
+						properties => {
+							persistent_homes => JSON::PP::true,
+							users => [
+								{
+									name => 'netop',
+									public_key => $self->_get_secret('op/net:public')
+								},
+								{
+									name => 'sysop',
+									crypted_password => $self->_get_secret('op/sys:password-crypt-sha512')
+								}
+							] } } ] } ]
+	};
+	my ($out, $rc, $err) = run(
+		'spruce merge <(echo "$1") $2',
+		JSON::PP::encode_json($ops_access_runtime),
+		$self->kit->path('overlay/releases/os-conf.yml')
+	);
+	bail("Failed to merge ops-access runtime: %s", $err) if $rc;
+	return $out;
+}
+
+sub _generate_merged_default_runtime {
+	my $self = shift;
+	my $runtime = {
+		addons => [
+			{name => 'genesis-local-users'},
+			{name => 'bosh-dns'}
+		]
+	};
+	my ($combined, $rc, $err) = run(
+		'spruce merge <(echo "$1") <(echo "$2") <(echo "$3")',
+		JSON::PP::encode_json($runtime),
+		$self->_generate_dns_runtime(),
+		$self->_generate_ops_access_runtime(),
+	);
+
+	bail("Failed to merge default runtime: %s", $err) if $rc;
+	return $combined;
+}
+
+sub perform {
+	my $self = shift;
+	my $env = $self->env;
+
+	# Parse options
+	my ($dryrun, $default, $removal) = (0, 0);
+	for (@{$self->{args}}) {
+		if ($_ eq '-n') {
+			$dryrun = 1;
+		} elsif ($_ eq '-y') {
+			$ENV{BOSH_NON_INTERACTIVE} = 1;
+		} elsif ($_ eq '-d') {
+			$default = 1;
+		} elsif ($_ eq '-R') {
+			$removal = 1;
+		} elsif ($_ =~ /^-/) {
+			bail("Bad option: $_");
+		} else {
+			bail("No arguments expected: $_");
+		}
+	}
+
+	# TODO: Add Clean up entombed secrets option
+
+	my %existing = map {
+		($_->{name} => {
+			since => $_->{created_at},
+			id => $_->{id} =~ s/\*$//r,
+			used => $_->{id} =~ /\*$/
+		})
+	} @{$self->_get_runtime_configs()};
+
+	$env->notify(
+		"%s runtime config%s", 
+		$removal ? "removing" : "generating",
+		$dryrun ? " (dry-run)" : ""
+	);
+
+	if ($removal) {
+		my @configs = $default
+			? ('default')
+			: ('genesis.bosh-dns', 'genesis.ops-access');
+		for my $config (@configs) {
+			if (!exists $existing{$config}) {
+				info("  - runtime config #g{%s} does not exist", $config);
+				next;
+			}
+			if ($dryrun) {
+				info(
+					"[[  - >>would remove %s runtime config #c{%s} (id: %s - %s)",
+					$existing{$config}{used} ? "#y{actve}" : "#g{unused}",
+					$config, $existing{$config}{id}, $existing{$config}{since}
+				);
+			} else {
+				my $start_time = time();
+				info({pending => 1}, "  - removing existing #g{%s} runtime...", $config);
+				my ($out,$rc,$err) = $self->_remove_runtime_config($config);
+				if ($rc) {
+					info("#R{failed}");
+					bail("Failed to remove runtime config: %s", $err);
+				} else {
+					info("#G{done}".pretty_duration(time() - $start_time));
+				}
+				info("#G{done}".pretty_duration(time() - $start_time));
+			}
+		}
+		info($dryrun ? "" : "  - runtime config removal complete\n");
+		return 1;
+	}
+
+
+	info({pending => 1}, "  - gathering current Exodus metadata...");
+	my %exodus;
+	my $t = time_exec(sub {
+		%exodus = %{$self->exodus_data()};
+	});
+	unless (keys %exodus) {
+		info("#R{failed}");
+		bail(
+			"Environment '#C{%s}' has not been deployed.  Please deploy it first, ".
+			"then rerun the 'runtime-config' addon",
+			$env->name
+		);
+	}
+	info("#G{done}".pretty_duration($t, 0.5, 1.0));
+
+	# Determine secrets to be used, ensure they exist, and entomb them if necessary
+	info({pending => 1},"[[  - >>looking up secrets for runtime config...");
+	my $start_time = time();
+	my @secrets =  qw(
+		dns_api_tls/ca:certificate
+		dns_api_tls/client:certificate
+		dns_api_tls/client:key
+		dns_api_tls/server:certificate
+		dns_api_tls/server:key
+	);
+	push @secrets, qw(
+		dns_healthcheck_tls/ca:certificate
+		dns_healthcheck_tls/client:certificate
+		dns_healthcheck_tls/client:key
+		dns_healthcheck_tls/server:certificate
+		dns_healthcheck_tls/server:key
+	) if ($self->want_feature('bosh-dns-healthcheck'));
+	push @secrets, 'op/net:public'
+		if ($self->want_feature('netop-access'));
+	push @secrets, 'op/sys:password-crypt-sha512'
+		if ($self->want_feature('sysop-access'));
+
+	my $secret_values = $env->secrets_store->store_data();
+	info("#G{done}".pretty_duration(time() - $start_time));
+	my $base_path = $env->secrets_store->base =~ s/^\///r;
+	my @missing_secrets = ();
+	for my $secret (@secrets) {
+		my ($path, $key) = split(/:/, $secret, 2);
+		if (!exists $secret_values->{$base_path.$path} || !exists $secret_values->{$base_path.$path}{$key}) {
+			push @missing_secrets, $secret;
+		}
+	}
+	bail("Missing secrets: %s", join(", ", @missing_secrets)) if @missing_secrets;
+
+	# Entombify the secrets if allowed - TODO: extract this to a helper
+	my $vault = $env->secrets_store->service;
+	if ($env->can_be_entombed) {
+		do $ENV{GENESIS_LIB}."/Genesis/Env/Manifest/_entombment_mixin.pm";
+		$vault = $self->_setup_local_vault();
+		my $credhub = $self->credhub();
+		$start_time = time();
+		info("[[  - >>entombifying %s secrets...", scalar @secrets);
+		my $idx = 0;
+		my $secrets_count = scalar @secrets;
+		my $w = length("$secrets_count");
+		my $previous_lines = 0;
+		for my $secret (sort @secrets) {
+			my ($path, $key) = split(/:/, $secret, 2);
+			my $value = $secret_values->{$base_path.$path}{$key};
+			my ($credhub_var, $secret_sha, $action, $action_color, $existing) = $self->_entomb_secret(
+				$vault, $base_path.$path, $key, $value, $credhub, $path, "/runtime-config/genesis-entombed/"
+			);
+			my $msg = wrap(sprintf(
+				"[[    [%*d/%*d] >>%s:#c{%s} #Kk{[sha1: }#Wk{%s}#Kk{]} #G{=>} #B{%s} ...#%s{%s}",
+				$w, ++$idx, $w, $secrets_count, "#C{$path}", $key, $secret_sha,
+				$credhub_var, $action_color, $action
+			), terminal_width);
+			print STDERR "\r[A[2K" for (1..$previous_lines);
+			info $msg;
+			$previous_lines=($existing && $existing eq $value) ? scalar(lines($msg)) : 0;
+		}
+		print STDERR "\r[A[2K" for (1..$previous_lines);
+		info("  - finished entombifying %s secrets%s", $secrets_count, pretty_duration(time() - $start_time));
+	}
+
+	# Generate the runtime config
+	$self->{vault} = $vault;
+	if($dryrun) {
+		if ($default) {
+			my $config = $self->_generate_merged_default_runtime();
+			info("[[  - >>would upload #c{default} runtime config:");
+			info("\n".render_markdown("```yaml\n$config\n```"));
+		} else {
+			my $dns_config = $self->_generate_dns_runtime();
+			info("[[  - >>would upload #c{genesis.bosh-dns} runtime config:");
+			info("\n".render_markdown("```yaml\n$dns_config\n```"));
+			if ($self->want_feature('netop-access') || $self->want_feature('sysop-access')) {
+				my $ops_config = $self->_generate_ops_access_runtime();
+				info("[[  - >>would upload #c{genesis.ops-access} runtime config:");
+				info("\n".render_markdown("```yaml\n$ops_config\n```"));
+			}
+		}
+	} else {
+		if ($default) {
+			my $config = $self->_generate_merged_default_runtime();
+			info("[[  - >>upload #c{default} runtime config:\n");
+			$self->_upload_runtime_config('default', $config);
+		} else {
+			my $dns_config = $self->_generate_dns_runtime();
+			info("[[  - >>upload #c{genesis.bosh-dns} runtime config:\n");
+			$self->_upload_runtime_config('genesis.bosh-dns', $dns_config);
+			if ($self->want_feature('netop-access') || $self->want_feature('sysop-access')) {
+				my $ops_config = $self->_generate_ops_access_runtime();
+				info("[[  - >>upload #c{genesis.ops-access} runtime config:\n");
+				$self->_upload_runtime_config('genesis.ops-access', $ops_config);
+			}
+		}
+	}
+
+	# FIXME: Clean up outdated entombed secrets if any
+	
+	# TODO: Set the result (defaults to 1 right now)
+	return 1;
+}
+
+1;
+#vim: fdm=marker:foldlevel=1:noet
---
+++ hooks/blueprint.pm
@@ -0,0 +1,402 @@
+#!/usr/bin/env perl
+package Genesis::Hook::Blueprint::Bosh v3.0.4;
+
+use strict;
+use warnings;
+
+# Only needed for development
+my $lib;
+BEGIN {$lib = $ENV{GENESIS_LIB} ? $ENV{GENESIS_LIB} : $ENV{HOME}.'/.genesis/lib'}
+use lib $lib;
+
+use parent qw(Genesis::Hook::Blueprint);
+
+use Genesis qw/bail info warning error in_array new_enough/;
+
+sub init {
+	my $class = shift;
+	my $obj = $class->SUPER::init(@_);
+	$obj->{features} = [$obj->env->features];
+	$obj->{files} = [];
+	$obj->check_minimum_genesis_version('3.0.0-rc.1');
+	return $obj;
+}
+
+sub perform {
+	my ($blueprint) = @_; # $blueprint is '$self'
+
+	$blueprint->{ocfp_env_type} = '';
+	if ($blueprint->want_feature('ocfp')) {
+		$blueprint->{ocfp_env_type} = ($blueprint->env->name =~ /.*-mgmt.*/)
+			? 'mgmt'
+			: 'ocf'
+	}
+
+	$blueprint->add_files(qw(
+		bosh-deployment/bosh.yml
+		bosh-deployment/uaa.yml
+		bosh-deployment/credhub.yml
+		bosh-deployment/misc/dns.yml
+		bosh-deployment/misc/ntp.yml
+		bosh-deployment/misc/proxy.yml
+		bosh-deployment/misc/trusted-certs.yml
+	));
+
+	# NOTE: This is until bosh-deployment is upgraded:
+	$blueprint->add_files("overlay/nats2.yml");
+
+	# Features pre-check: Check for ops features
+	my (@features,$iaas,$db,$abort,$warn) = ();
+	for my $feature ($blueprint->features) {
+		if ($feature =~ /^(aws|azure|google|openstack|vsphere|warden)-cpi$/) {
+			my $trimmed_feature = $1;
+			$warn = 1;
+			warning(
+				"The #c{%s} feature has been renamed to #c{%s}",
+				$feature, $trimmed_feature
+			);
+			if ($iaas) {
+				$abort = 1;
+				error(
+					"The #c{%s} feature cannot be used because #c{%s} is already ".
+					"selected as the cloud provider.",
+					$trimmed_feature, $iaas
+				)
+			} else {
+				$iaas = $trimmed_feature;
+				push @features, $trimmed_feature;
+			}
+		} elsif ($feature =~ /^(aws|azure|google|openstack|vsphere|bosh)-init$/) {
+			my $trimmed_feature = $1;
+			$warn = 1;
+			warning(
+				"The #c{%s} feature is now two separate features: #c{%s} and #c{proto}",
+				$feature, $trimmed_feature
+			);
+			if ($iaas) {
+				$abort = 1;
+				error(
+					"The #c{%s} feature cannot be used because #c{%s} is already ".
+					"selected as the cloud provider.",
+					$trimmed_feature, $iaas
+				)
+			} else {
+				$iaas = $trimmed_feature;
+				push @features, $trimmed_feature, 'proto';
+			}
+		} elsif ($feature =~ /^(aws|azure|google|openstack|vsphere|warden)$/) {
+			if ($iaas) {
+				$abort = 1;
+				error(
+					"The #c{%s} feature cannot be used because #c{%s} is already ".
+					"selected as the cloud provider.",
+					$feature, $iaas
+				)
+			} else {
+				$iaas = $feature;
+				push @features, $feature;
+			}
+		} elsif ($feature eq 'ocfp') {
+			if ($db) {
+				$abort = 1;
+				error(
+					"The #c{%s} feature cannot be used because #c{%s} is already ".
+					"selected as the database.",
+					$feature, $db
+				);
+			} else {
+				$db = $feature;
+				push @features, $feature;
+			}
+		} elsif ($feature =~ /^external-db(?:|-(mysql|postgres))$/) {
+			if ($db) {
+				$abort = 1;
+				error(
+					"The #c{%s} feature cannot be used because #c{%s} is already ".
+					"selected as the database.",
+					$feature, $db
+				);
+			} else {
+				$db = $feature;
+				push @features, $feature;
+			}
+		} elsif ($feature eq 'shield') {
+			$warn = 1;
+			warning(
+				"The #c{shield} feature is no longer supported.  Instead, please add ".
+				"the shield agent to your runtime configuration."
+			);
+		} elsif ($feature eq 'external-db-ca') {
+			$warn = 1;
+			warning(
+				"The functionality contained within the 'external-db-ca' has become ".
+				"mandatory and has therefore been merged into the base kits ".
+				"#c{external-db-postgres} and #c{external-db-mysql}. You can remove ".
+				"the 'external-db-ca' feature without any changes occurring."
+			);
+		} elsif ($feature =~ /^(proxy|credhub|registry)$/) {
+			$warn = 1;
+			warning(
+				"You no longer need to explicitly specify the #c{%s} feature.  If you ".
+				"remove it, everything will still work as expected."
+			);
+		} elsif (in_array($feature, qw(
+			proto skip-op-users vault-credhub-proxy external-db-no-tls okta
+			s3-blobstore iam-instance-profile s3-blobstore-iam-instance-profile
+			minio-blobstore node-exporter trust-blacksmith-ca source-releases
+			blacksmith-integration doomsday-integration bosh-metrics bosh-lb
+			bosh-dns-healthcheck netop-access sysop-access
+		))) {
+			push @features, $feature
+		} elsif ($feature =~ /^\+/) {
+			# virtual feature dynamically created based on other features/params
+			push @features, $feature
+		} elsif ($feature =~ /^bosh-deployment\/.*/) {
+			if (in_array($feature, $blueprint->files)) {
+				warning(
+					"%s is already included in the base manifest, and should not be ".
+					"listed in the features list.",
+					$feature
+				);
+				next;
+			}
+			if (-f "${feature}.yml") {
+				push @features, $feature;
+			} else {
+				$abort = 1;
+				error(
+					"#c{%s} is not an upstream operation -- see bosh-deployment for ".
+					"valid operations.",
+					$feature
+				);
+			} 
+		} elsif ( -f $blueprint->env->path("ops/${feature}.yml")) {
+			push @features, $feature
+		} else {
+			$abort = 1;
+			error(
+				"The #c{%s} feature is invalid. See the manual for list of valid ".
+				"features.",
+				$feature
+			)
+		}
+	}
+
+	# Check validity of given features
+	unless (defined($iaas)) {
+		$abort = 1;
+		error(
+			"No specified IaaS feature for this environment, expecting one of: aws, ".
+			"azure, google, openstack, vsphere or warden"
+		)
+	}
+	
+	bail(
+		"#R{Cannot continue} - fix your #C{%s} file to resolve these issues.",
+		$blueprint->relative_env_path,
+	) if $abort;
+	info(
+		"Update your #C{%s} file to remove these warnings.\n",
+		$blueprint->relative_env_path
+	) if $warn;
+
+	# Replace given features with the currated list
+	$blueprint->set_features(@features);
+
+	$blueprint->add_files('overlay/base-proto.yml') if $blueprint->is_create_env;
+	$blueprint->add_files(qw(
+		overlay/base.yml
+		overlay/upstream_version.yml
+		overlay/addons/credhub.yml
+	));
+
+	# use source-releases instead of compiled releases
+	$blueprint->add_files(qw(
+		bosh-deployment/misc/source-releases/bosh.yml
+		bosh-deployment/misc/source-releases/credhub.yml
+		bosh-deployment/misc/source-releases/uaa.yml
+	)) if $blueprint->want_feature('source-releases');
+
+	$blueprint->add_files(qw(
+		bosh-deployment/jumpbox-user.yml
+		overlay/addons/op-users.yml
+	)) unless $blueprint->want_feature('skip-op-users');
+
+	for my $feature ($blueprint->features) {
+		if ($feature eq "warden") {
+			bail(
+				"BOSH Warden CPI can not be deployed as a proto-BOSH"
+			) if $blueprint->is_create_env;
+			$blueprint->add_files(qw(
+				bosh-deployment/bosh-lite.yml
+				overlay/cpis/warden.yml
+				overlay/no-proto.yml
+			));
+		} elsif ($feature =~ /^(aws|azure|google|openstack|vsphere)$/) {
+			my $cpi = ($feature eq 'google') ? 'gcp' : $feature;
+			$blueprint->add_files(
+				"bosh-deployment/${cpi}/cpi.yml",
+				"overlay/cpis/${cpi}.yml"
+			);
+			$blueprint->add_files(
+				"bosh-deployment/${cpi}/use-managed-disks.yml"
+			) if $cpi eq 'azure';
+      $blueprint->add_files(
+				($blueprint->is_create_env) 
+				? "overlay/cpis/${cpi}-proto.yml"
+				: "overlay/no-proto.yml"
+			);
+		} elsif ($feature eq 'iam-instance-profile') {
+			bail(
+				"Cannot use IAM instance profiles if not deploying to AWS"
+			) if $iaas eq 'aws';
+			$blueprint->add_file("overlay/addons/iam-profile.yml");
+		} elsif ($feature eq 's3-blobstore') {
+			$blueprint->add_files(qw(
+				bosh-deployment/aws/s3-blobstore.yml
+				overlay/addons/s3-blobstore.yml
+			));
+			if ($blueprint->want_feature("s3-blobstore-iam-instance-profile")) {
+				bail(
+					"Cannot use IAM instance profiles if not deploying to AWS"
+				) if $iaas eq 'aws';
+				$blueprint->add_files("overlay/addons/s3-blobstore-iam-profile.yml");
+			}
+		} elsif ($feature eq 'minio-blobstore') {
+			bail(
+				"Can only specify one of: s3-blobstore, minio-blobstore"
+			) if $blueprint->want_feature('s3-blobstore');
+			$blueprint->add_files("overlay/addons/minio-blobstore.yml");
+		} elsif ($feature eq '+internal-blobstore') {
+			$blueprint->add_files("overlay/addons/internal-blobstore.yml")
+		} elsif ($feature =~ /^(external-db-mysql|external-db-postgres)$/) {
+			$blueprint->add_files(qw(
+				overlay/addons/external-db-internal-db-cleanup.yml
+				overlay/addons/external-db.yml
+			));
+			if ($feature eq "external-db-mysql") {
+				$blueprint->add_files('overlay/addons/external-db-mysql.yml');
+			}
+			$blueprint->add_files(
+				$blueprint->want_feature("external-db-no-tls")
+					? "overlay/addons/external-db-no-tls.yml"
+					: "overlay/addons/external-db-ca.yml"
+			);
+		} elsif ($feature eq 'trust-blacksmith-ca') {
+			$blueprint->add_files(
+				$blueprint->want_feature("ocfp")
+				?	"ocfp/trust-blacksmith-ca.yml"
+				:"overlay/addons/trust-blacksmith-ca.yml"
+			);
+		} elsif ($feature eq 'ocfp') {
+			if ($iaas eq 'aws') {
+				$blueprint->add_files( 
+					"ocfp/remove-internal-blobstore.yml",
+					"bosh-deployment/aws/s3-blobstore.yml" 
+				)
+			} elsif ($iaas eq 'google') {
+				$blueprint->add_files( 
+					"ocfp/remove-internal-blobstore.yml",
+					"bosh-deployment/gcp/gcs-blobstore.yml" 
+				)
+			} else {
+				$blueprint->kit->kit_bug(
+					"The ocfp feature has not been implemented for the $iaas ".
+					"infrastructure"
+				)
+			}
+
+			my $env_type = $blueprint->{ocfp_env_type};
+			$blueprint->add_files(
+				"ocfp/meta.yml",
+				"ocfp/ocfp.yml",
+				"ocfp/${iaas}/${env_type}.yml",
+				"overlay/addons/external-db-internal-db-cleanup.yml",
+				"ocfp/external-db.yml"
+			);
+
+			$blueprint->add_files(
+				"overlay/addons/external-db-no-tls.yml"
+			) if $blueprint->want_feature("external-db-no-tls");
+
+			$blueprint->add_files(
+				"ocfp/bosh-lb.yml"
+			) if $blueprint->want_feature("bosh-lb");
+		} elsif (basic_feature($feature)) {
+			$blueprint->add_files("overlay/addons/${feature}.yml")
+		} elsif (noop_feature($feature)) {
+			# Do nothing
+		} elsif ($feature =~ /^bosh-deployment\/.*/) {
+			warning(
+				"Upstream $feature is already included in the manifest, possibly as ".
+				"part of another feature.  Please remove it from the #yi{kit.features} ".
+				"list."
+			) if (in_array($feature, $blueprint->files));
+			$blueprint->add_files("$feature");
+		} elsif ( -f $blueprint->env->path("ops/${feature}.yml")) {
+			push @features, $feature
+		} else {
+			$abort = 1;
+			error(
+				"The #c{%s} feature is invalid. See the manual for list of valid ".
+				"features.",
+				$feature
+			)
+		}
+	}
+
+	# Cleanup
+	if ($blueprint->is_create_env) {
+		# If this is a `create-env` BOSH and one of the iam-instance-profile or
+		# s3-blobstore-iam-instance-profile features are requested, then we need
+		# to ensure the proto-BOSH has the correct cloud properties
+		$blueprint->add_files(
+			"bosh-deployment/aws/cli-iam-instance-profile.yml",
+			"overlay/addons/proto-iam-profile.yml"
+		) if $blueprint->want_feature("iam-instance-profile") 
+			|| $blueprint->want_feature("s3-blobstore-iam-instance-profile");
+	}
+
+	# Use params.availability_zones if set, otherwise default to "z1"
+	$blueprint->add_files("overlay/set-availability-zone.yml");
+
+	# Upgrade check
+	my $prev_version = $blueprint->env->exodus_lookup("kit_version","");
+	bail(
+		"Detected previous deployment of BOSH kit v%s- please upgrade to at ".
+		"least bosh kit 2.3.0 before upgrading to > 3.0.0"
+	) if ($prev_version ne "" && ! new_enough($prev_version,"2.2.7-rc.0"));
+
+	return $blueprint->done;
+}
+
+# Support methods
+my $_basic_features = {
+	map {($_,1)} qw(
+		vault-credhub-proxy node-exporter bosh-metrics okta blacksmith-integration
+		doomsday-integration) };
+sub basic_feature { return $_basic_features->{$_[0]}; }
+
+my $_noop_features = {
+	map {($_,1)} qw(
+		proto source-releases s3-blobstore-iam-instance-profile external-db-no-tls
+		skip-op-users bosh-dns-healthcheck netop-access sysop-access
+		+aws-secret-access-keys +s3-blobstore-secret-access-keys +external-db) };
+sub noop_feature { return $_noop_features->{$_[0]} }
+
+sub is_create_env {
+	my $self = shift;
+
+	# v2.8.x method - overrides all other methods
+	my $use_create_env = $self->env->lookup('genesis.use_create_env');
+	if ($use_create_env) {
+		return !!($use_create_env =~ /^(1|enable|on|true|yes)$/);
+	} elsif ($self->{ocfp_env_type} eq "mgmt") {
+		# OCFP management automatic method
+		return 1;
+	} 
+
+	# Classic method
+	$self->want_feature("proto")
+}
+1;
