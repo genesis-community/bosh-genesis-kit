00c5aa0fb38b4474fcd542b5e402a6b8d0b74de5
Dennis J. Bell <dennis.j.bell@gmail.com>
2024-11-22 02:03:15 -0800

    Fix the post-deploy hook
    - Fix the signature of get_target_bosh
    - Fix the signature of upload_config
    - Call get_target_bosh and vault methods on the environment object

--- hooks/post-deploy.pm
+++ hooks/post-deploy.pm
@@ -38,15 +38,15 @@ sub perform {
 		# FIXME: Do a check and compare, and ask if different (or just do it if $BOSH_NON_INTERACTIVE is set)
 		# or at least show the diff (maybe too late to ask if bosh is already deployed)
 		info({pending => 1}, "[[  - >>applying director cloud-config...");
-		my $bosh = $self->get_target_bosh(self => 1);
+		my $bosh = $env->get_target_bosh({$self => !$env->use_create_env});
 		my $config_name = join('.',$env->name, $env->type, 'director');
 		$tstart = gettimeofday;
-		$bosh->upload_config('cloud', $config_name, $config);
+		$bosh->upload_config($config, 'cloud', $config_name);
 		info("#G{done}" . pretty_duration(gettimeofday - $tstart, 5, 10));
 
 		info({pending => 1}, "[[  - >>storing director network in exodus...");
 		$tstart = gettimeofday;
-		$self->vault->set_path($env->exodus_base.'/networks', $network, flatten => 1);
+		$env->vault->set_path($env->exodus_base.'/networks', $network, flatten => 1);
 		info("#G{done}" . pretty_duration(gettimeofday - $tstart, 1, 3));
 		info('[[  - >>#M{%s} BOSH Director cloud-config applied.', $env->name);
 
@@ -58,7 +58,7 @@ sub perform {
 
 		# Provide usage assistance (aka help)
 		info(
-			"For details about the deployment, run\n".
+			"\nFor details about the deployment, run\n".
 			"  #G{$ENV{GENESIS_CALL_ENV} info}\n".
 			"\n".
 			"To run bosh command against this BOSH director, as the admin user, run\n".
