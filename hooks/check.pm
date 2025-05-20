532f0cc007a7510e57b0f3eac5ed471bd4eae4da
Dennis J. Bell <dennis.j.bell@gmail.com>
2025-05-20 14:10:37 -0700

    Simplified `hooks/check.pm` with Check.pm helpers
    - Updated `init` method to use a newer Genesis version (rc24 provides the Hook/Check.pm package).
    - Simplified `perform` method to streamline checks and return results.
    - Refactored `check_cloud_config` to use consistent entry validation:
      - Bypass for OCFP and create env.
      - Error if missing cloud config.
      - Generalized entry validation for `vm_type`, `network`, and `disk_type` for classic mode.
      - Used `start_check` and `check_result` for consistent check reporting.
    - Enhanced `check_environment_parameters` to validate vsphere datastore parameters as arrays.
      - Used `has_entry` for parameter validation for vsphere datastores.
      - Used `start_check` and `check_result` for consistent check reporting.
    - Improved `check_version_compatibility` to handle version upgrade checks more robustly.
      - Made uniform with other checks style.

--- hooks/check.pm
+++ hooks/check.pm
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# vim: set ts=2 sw=2 sts=2 et:
+# vim: set ts=2 sw=2 sts=2 noet:
 package Genesis::Hook::Check::Bosh v3.3.0; # version of the bosh kit
 
 use strict;
@@ -10,132 +10,94 @@ use v5.20; # Genesis supports min perl v5.20.
 BEGIN {push @INC, $ENV{GENESIS_LIB} ? $ENV{GENESIS_LIB} : $ENV{HOME}.'/.genesis/lib'}
 
 # Parent class inheritance
-use parent qw(Genesis::Hook);
+use parent qw(Genesis::Hook::Check);
 
 # Import required functions
 use Genesis qw/bail info warning error in_array new_enough/;
 
 sub init {
-  my ($class, %ops) = @_;
-  my $obj = $class->SUPER::init(%ops);
-  $obj->{ok} = 1; # Start assuming all checks will pass
-	$obj->{files} = [];
-	$obj->check_minimum_genesis_version('3.1.0-rc.9');
-  return $obj;
+	my ($class, %ops) = @_;
+	my $obj = $class->SUPER::init(%ops);
+	$obj->check_minimum_genesis_version('3.1.0-rc.24');
+	return $obj;
 }
 
 sub perform {
-  my ($self) = @_;
+	my ($self) = @_;
+	my $ok = 1;
 
-  # Check Genesis version
-  $self->check_genesis_version();
+	# Version compatibility checks
+	$ok = 0 unless $self->check_version_compatibility();
 
-  # Cloud Config checks
-  $self->check_cloud_config() if $ENV{GENESIS_CLOUD_CONFIG};
+	# Cloud Config checks
+	$ok = 0 unless $self->check_cloud_config();
 
-  # Environment Parameter checks
-  $self->check_environment_parameters();
+	# Environment Parameter checks
+	$ok = 0 unless $self->check_environment_parameters();
 
-  # Version compatibility checks
-  $self->check_version_compatibility();
-
-  # Return the final result
-  if ($self->{ok}) {
-    $self->env->notify(success => "environment files [#G{OK}]");
-  } else {
-    $self->env->notify(error => "environment files [#R{FAILED}]");
-  }
-
-  return $self->done($self->{ok});
-}
-
-sub check_genesis_version {
-  my ($self) = @_;
-
-  unless (new_enough($Genesis::VERSION, "2.8.6")) {
-    $self->{ok} = 0;
-    trace("Genesis version check failed - need at least 2.8.6");
-  }
+	return $self->done($ok);
 }
 
 sub check_cloud_config {
-  my ($self) = @_;
+	my ($self) = @_;
 
-  my $env = $self->env;
-  my ($vm_type, $network, $disk_type);
+	$self->start_check('cloud_config');
 
-  if ($env->want_feature("ocfp")) {
-    my $prefix = $env->lookup("params.cloud_config_prefix");
-    $prefix = "$env->{name}.$env->{type}" unless $prefix;
+	return $self->check_result('cloud-config', 'skipped', "create env") if $self->use_create_env;
+	return $self->check_result('cloud-config', 'skipped', "OCFP env") if $self->is_ocfp;
+	return $self->check_result('cloud-config', 'failed', "no cloud config found") if $self->env->has_config('no_cloud');
 
-    $vm_type = "$prefix.vm-bosh";
-    $network = "$prefix.net-bosh";
-    $disk_type = "$prefix.disk-bosh";
-  } else {
-    # Legacy was hard coded
-    $vm_type = "large";
-    $network = "bosh";
-    $disk_type = "bosh";
-  }
+	my ($vm_type, $network, $disk_type);
+	my $env = $self->env;
 
-  if (!$env->use_create_env) {
-    # Call the cloud_config_needs function for each requirement
-    # These appear to be implemented elsewhere in Genesis
-    run("cloud_config_needs vm_type \"" . $env->lookup("params.bosh_vm_type", $vm_type) . "\"");
-    run("cloud_config_needs network \"" . $env->lookup("params.bosh_network", $network) . "\"");
-    run("cloud_config_needs disk_type \"" . $env->lookup("params.bosh_disk_pool", $disk_type) . "\"");
+	# Legacy was hard coded
+	$vm_type = "large";
+	$network = "bosh";
+	$disk_type = "bosh";
 
-    if (system("check_cloud_config") == 0) {
-      $self->env->notify("  cloud-config [#G{OK}]");
-    } else {
-      $self->env->notify("  cloud-config [#R{FAILED}]");
-      $self->{ok} = 0;
-    }
-  }
+	$self->has_entry('cloud-config','vm_type', $vm_type);
+	$self->has_entry('cloud-config','network', $network);
+	$self->has_entry('cloud-config','disk_type', $disk_type);
+	return $self->check_result('cloud-config');
 }
 
 sub check_environment_parameters {
-  my ($self) = @_;
+	my ($self) = @_;
 
-  my $env = $self->env;
-
-  if ($env->want_feature("vsphere")) {
-    for my $ds_type (qw(ephemeral persistent)) {
-      my $param_name = "params.vsphere_${ds_type}_datastores";
-      my $type = $env->lookup("$param_name.__type", '');
-
-      if ($type ne "list") {
-        $type = ref($env->lookup($param_name)) eq 'ARRAY' ? 'list' : $type || 'scalar';
-        $self->env->notify("  ${ds_type} vsphere datastores is a #Y{$type}, not a list [#R{FAILED}]");
-        $self->{ok} = 0;
-      } else {
-        $self->env->notify("  ${ds_type} vsphere datastores checks out [#G{OK}]");
-      }
-    }
-  }
+	$self->start_check('environment');
+	if ($self->want_feature("vsphere")) {
+		for my $ds_type (qw(ephemeral persistent)) {
+			my $param_name = "vsphere_${ds_type}_datastores";
+			$self->has_entry('environment', 'params', $param_name, type => 'array', msg => 'is an array');
+		}
+	} else {
+		# TODO: Should we just skip environment checks if vsphere is not enabled, with no output?
+		return $self->check_result('environment', 'skipped', "no vsphere feature");
+	}
 }
 
 sub check_version_compatibility {
-  my ($self) = @_;
-
-  my $env = $self->env;
-  my $version = $env->exodus_lookup("kit_version", "");
-
-  if ($version) {
-    if (!new_enough($version, "3.0.0")) {
-      $self->env->notify("  #C{[Checking Upgrade from $version]}");
-
-      if (!new_enough($version, "2.2.7-rc.0")) {
-        $self->env->notify("    #R{[ERROR]} Please upgrade to at least bosh kit 2.3.0 before upgrading to v3.x.x");
-
-        if ($ENV{FORCE_INCOMPATIBLE_UPGRADE}) {
-          $self->env->notify("    #y{[WARN]} Forcing incompatible upgrade due to FORCE_INCOMPATIBLE_UPGRADE being set");
-        } else {
-          $self->{ok} = 0;
-        }
-      }
-    }
-  }
+	my ($self) = @_;
+	my $last_version = $self->exodus_data->{kit_version};
+	if ($last_version) {
+		if (!new_enough($last_version, "3.0.0")) {
+			$self->start_check('version upgrade compatibility');
+			if (!new_enough($last_version, "2.3.0")) {
+				return $self->check_result(
+					'version upgrade compatibility',
+					'warning',
+					"Forcing incompatible upgrade due to FORCE_INCOMPATIBLE_UPGRADE being set",
+				) if $self->env->exodus_lookup('FORCE_INCOMPATIBLE_UPGRADE', '');
+				return $self->check_result(
+					'version upgrade compatibility',
+					'failed',
+					"Please upgrade to at least bosh kit 2.3.0 before upgrading to v3.x.x",
+				);
+			}
+			return $self->check_result('version upgrade compatibility', 'passed')
+		}
+	}
+	return 1;
 }
 
 1;
