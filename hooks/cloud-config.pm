df1642d9ea717a379e298de37937c18b8ae77138
Wayne E. Seguin <wayneeseguin@gmail.com>
2025-05-21 11:40:32 -0400

    Added AWS to cloud-config.pm

--- hooks/cloud-config.pm
+++ hooks/cloud-config.pm
@@ -28,11 +28,23 @@ sub perform {
 	# we need to use custom AZs that are a shadow version of the parent AZs that
 	# use the CPI (if not create-env), and then the network has to use those AZs.
 
-
 	my $config = $self->build_cloud_config({
 		$self->build_cpi_azs(
 			virtual => scalar($self->env->lookup('bosh-configs.virtual_azs', $self->FALSE)),
 		),
+		'vm_extensions' => [
+			{
+				'name' => 'bosh-lb',
+				'cloud_properties' => {
+					aws => {
+						'lb_target_groups' => [$self->env->lookup(
+							'cloud-config.bosh-lb-target-group',
+							'ocfp-' . ( $ENV{GENESIS_ENVIRONMENT} || 'mgmt' ) . '-bosh-lb-tg'
+						)]
+					}
+				}
+			}
+		],
 		'networks' => [
 			# FIXME: strategy should be defined by the environment, not the kit
 			$self->network_definition('bosh', strategy => 'ocfp',
@@ -42,6 +54,11 @@ sub perform {
 						statics => 0,
 					},
 					cloud_properties_for_iaas => {
+						aws => {
+              #'net_id' => $self->network_reference('id'),
+							'subnet' => $self->subnet_reference('id')
+							'security_groups' => ['default'] #$self->subnet_reference('sgs', 'get_security_groups'),
+						},
 						openstack => {
 							'net_id' => $self->network_reference('id'), # TODO: $self->subnet_reference('net_id'),
 							'security_groups' => ['default'] #$self->subnet_reference('sgs', 'get_security_groups'),
@@ -58,6 +75,23 @@ sub perform {
 		'vm_types' => [
 			$self->vm_type_definition('bosh',
 				cloud_properties_for_iaas => {
+					aws => {
+						'instance_type' => $self->for_scale({
+							dev => 't3.large',
+							prod => 'm6i.2xlarge'
+						}, 't3.large'),
+						'ephemeral_disk' => {
+							'size' => $self->for_scale({
+								dev => gigabytes(64),
+								prod => gigabytes(128)
+							}, gigabytes(64)),
+							'type' => 'gp3',
+							'encrypted' => $self->TRUE
+						},
+						'metadata_options' => {
+							'http_tokens' => 'required'
+						}
+					},
 					openstack => {
 						'instance_type' => $self->for_scale({
 							dev => 'm1.2',
@@ -85,11 +119,15 @@ sub perform {
 			$self->disk_type_definition('bosh',
 				common => {
 					disk_size => $self->for_scale({ # add $self->for_feature('internal-blobstore')
-						dev => gigabytes(64),
-						prod => gigabytes(128)
-					}, gigabytes(96)),
+						dev => gigabytes(32),
+						prod => gigabytes(256)
+					}, gigabytes(32)),
 				},
 				cloud_properties_for_iaas => {
+					aws => {
+						'type' => 'gp3',
+						'encrypted' => $self->TRUE
+					},
 					openstack => {
 						'type' => 'storage_premium_perf6',
 					},
