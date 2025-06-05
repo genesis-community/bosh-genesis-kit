6f9c29e79d2f2235b4078998b1c0bbdff384f6a1
Dennis J. Bell <dennis.j.bell@gmail.com>
2025-06-05 00:11:17 -0700

    Perl Hooks Finalized: info
    Refactored info hook to follow Genesis v3 Perl hook standards:
    
    - Removed shebang and updated package declaration format
    - Standardized indentation to use tabs with 2-space width
    - Added proper fold markers for method documentation
    - Reorganized imports to match coding standards
    - Improved error handling for missing exodus data with warnings
    - Enhanced output formatting with better color coding and structure
    - Added comprehensive usage notes for bosh and credhub commands
    - Consolidated exodus data retrieval into single hash operation
    - Added missing vim configuration line at end of file

--- hooks/info.pm
+++ hooks/info.pm
@@ -1,82 +1,115 @@
-#!/usr/bin/env perl
-# # vim: set ts=2 sw=2 sts=2 et:
-package Genesis::Hook::Info::Bosh v3.3.0; # ...::[KIT] v[KIT_VERSION]
+package Genesis::Hook::Info::Bosh v3.3.0;
 
-use strict;
+use v5.20; # Genesis min perl version is 5.20
 use warnings;
-use v5.20; # Genesis supports min perl v5.20.
 
 # Only needed for development
 BEGIN {push @INC, $ENV{GENESIS_LIB} ? $ENV{GENESIS_LIB} : $ENV{HOME}.'/.genesis/lib'}
 
-# Parent class inheritance
 use parent qw(Genesis::Hook);
 
-# Import required functions
-use Genesis qw/bail info warning error in_array new_enough run/;
+use Genesis qw/bail info run warning/;
 
+# init - Initialize the hook {{{
 sub init {
-  my ($class, %ops) = @_;
-  my $obj = $class->SUPER::init(%ops);
+	my ($class, %ops) = @_;
+	my $obj = $class->SUPER::init(%ops);
 	$obj->check_minimum_genesis_version('3.1.0-rc.9');
-  return $obj;
+	return $obj;
 }
 
+# }}}
+
+# perform - Main hook execution {{{
 sub perform {
-  my ($self) = @_;
+	my ($self) = @_;
 
-  # Get BOSH environment info from exodus data
-  my $bosh_environment = $self->env->exodus_lookup('url') || 'https://127.0.0.1:25555';
-  my $bosh_ca_cert = $self->env->exodus_lookup('ca_cert');
-  my $bosh_client = $self->env->exodus_lookup('admin_username');
-  my $bosh_client_secret = $self->env->exodus_lookup('admin_password');
+	# Display access instructions
+	my $call_with_env = $self->env->get_call_path_with_env();
 
-  # Display BOSH environment
-  info("BOSH env");
-  my ($out, $rc, $err) = run("bosh -A env --tty | sed -e 's/^/  /'");
-  info($out);
+	# Gather the necessary information from Exodus
+	my %needed_exodus_data = map {($_, $self->exodus_data->{$_}//undef)} qw(
+		url
+		admin_username
+		admin_password
+		ca_cert
+		credhub_url
+		has_vault_credhub_proxy
+	);
 
-  # Display access instructions
-  my $call_with_env = $self->env->get_call_path_with_env();
+	my @missing_exodus_fields = grep {!defined($needed_exodus_data{$_})} keys %needed_exodus_data;
+	warning(
+		"\nMissing the following data from the last deploy:\n%s\n\n".
+		"Please redeploy in order to generate the necessary information.\n",
+		join("\n", map { "[[  - >>$_" } @missing_exodus_fields)
+	) if @missing_exodus_fields;
 
-  info(join("\n",
-      "",
-      "Accessing BOSH:",
-      "",
-      "To log into the BOSH director from the command line:",
-      "  #G{$call_with_env do -- login}",
-      "",
-      "If you need to provide the access credentials, they are:",
-      "",
-      "  #i{    bosh url:} #C{$bosh_environment}",
-      "  #i{    username:} #C{$bosh_client}",
-      "  #i{    password:} #C{$bosh_client_secret}",
-      "#i{ca certificate:}",
-      "#C{$bosh_ca_cert}",
-      ""
-    ));
+	my %info = %needed_exodus_data;
 
-  # Check for Credhub
-  my $bosh_credhub_url = $self->env->exodus_lookup('credhub_url');
-  if ($bosh_credhub_url) {
-    info(join("\n",
-        "To log into the Credhub provided by this BOSH deployment:",
-        "  #G{$call_with_env do -- credhub-login}",
-        ""
-      ));
-  }
+	# Display BOSH environment
+	info("#B{BOSH Director Information}\n");
+	my ($out, $rc, $err) = run("bosh -A env --tty | sed -e 's/^/  /'");
+	info($out) if $rc == 0;
 
-  # Check for vault-credhub-proxy
-  my $has_vault_credhub_proxy = $self->env->exodus_lookup('has_vault_credhub_proxy');
-  if ($has_vault_credhub_proxy) {
-    info(join("\n",
-        "To log into the Credhub via #C{safe} using the vault-credhub-proxy:",
-        "  #G{$call_with_env do -- vault-proxy-login}",
-        ""
-      ));
-  }
+	info(
+		"\nBOSH Director endpoint information\n".
+		"[[  >>#C{%s}\n\n".
+		"BOSH Director credentials\n".
+		"[[  >>username: #M{%s}\n".
+		"[[  >>password: #G{%s}\n\n".
+		"BOSH Director CA Certificate:\n".
+		"#C{%s}\n\n",
+		"To log into the BOSH director from the command line:\n".
+		"[[  >>#G{%s do -- login}\n\n".
+		"[[#Yiu{Note:} >>While the above method will allow you to log into the ".
+		"BOSH director, it is recommended to use #G{%s bosh <cmd> <options and ".
+		"arguments>}.  Doing so will allow you to easily switch between BOSH ".
+		"directors, and will automatically set the BOSH environment, and ".
+		"in the case of a non-BOSH environment (ie cf, vault, jumpbox), the ".
+		"deployment as well.  When calling on a BOSH environment, you can ".
+		"specify #Y{--self} to use that environment as the BOSH director, or ".
+		"#Y{--parent} to target the director that deployed it, with the current ".
+		"environment as the deployment.\n\n",
+		$info{url}            // '}#R{<unknown>',
+		$info{admin_username} // '}#R{<unknown>',
+		$info{admin_password} // '}#R{<unknown>',
+		$info{ca_cert},
+		($call_with_env) x 2
+	);
 
-  return $self->done();
+	# Check for Credhub
+	if ($info{credhub_url}) {
+		info(
+			"\nTo log into the Credhub provided by this BOSH deployment:\n".
+			"[[  >>#G{%s do credhub-login}\n\n".
+			"#Yiu{Note:} >>Likewise with logging into the BOSH director, ".
+			"you can use #G{%s credhub <cmd> <options and arguments>} to ".
+			"interact with the Credhub, and it will automatically set the ".
+			"credhub base path to the current environment, so rather than ".
+			"logging into the Credhub on the BOSH environment, you can ".
+			"call Credhub commands on the desired environment (ie cf) and ".
+			"it will automatically get the credentials from the Credhub ".
+			"on the BOSH director that deployed it.\n\n",
+			($call_with_env) x 1
+		);
+	}
+
+	# Check for vault-credhub-proxy
+	if ($info{has_vault_credhub_proxy}) {
+		info(
+			"\nTo log into the Credhub via #C{safe} using the vault-credhub-proxy:\n".
+			"[[  >>#G{%s do vault-proxy-login}\n\n".
+			"This will set up your ~/.saferc file to use the vault-credhub-proxy as ".
+			"a vault server, and will allow you to use the #C{safe} command ".
+			"to interact with the Credhub on the BOSH director.\n\n",
+			$call_with_env
+		);
+	}
+
+	return $self->done();
 }
+
+# }}}
 
 1;
+# vim: set ts=2 sw=2 sts=2 noet fdm=marker foldlevel=1:
