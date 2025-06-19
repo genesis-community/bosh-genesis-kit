8b36b855f159c807f29280f6d1242b930954387f
Dennis J. Bell <dennis.j.bell@gmail.com>
2025-06-19 00:52:09 -0700

    Update cloud-config hook to use dynamic cloud properties
    The `cloud-config.pm` hook now dynamically determines cloud properties
    for the `bosh-lb` VM extension based on the IaaS provider. This replaces
    the static `cloud_properties` key with a call to the new method
    `_cloud_properties_for_iaas`.
    
    This change improves flexibility and reduces hardcoding in cloud
    configuration generation.  Before it would put an invalid configuration
    for cloud_properties that included an IaaS key with the actual
    properties, now it will use the IaaS key only when needed.

--- hooks/cloud-config.pm
+++ hooks/cloud-config.pm
@@ -35,14 +35,14 @@ sub perform {
 		'vm_extensions' => [
 			{
 				'name' => 'bosh-lb',
-				'cloud_properties' => {
+				$self->_cloud_properties_for_iaas(
 					aws => {
 						'lb_target_groups' => [$self->env->lookup(
 							'cloud-config.bosh-lb-target-group',
 							'ocfp-' . ( $ENV{GENESIS_ENVIRONMENT} || 'mgmt' ) . '-bosh-lb-tg'
 						)]
 					}
-				}
+				)
 			}
 		],
 		'networks' => [
