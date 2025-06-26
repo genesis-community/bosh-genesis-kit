32fbb2c68255d3c1516367cd472a0575eed3ee0a
Kevin Rutten <krutten@fivetwenty.io>
2025-06-26 04:09:11 +0000

    Update CPI to 17

--- hooks/info.pm
+++ hooks/info.pm
@@ -8,7 +8,7 @@ BEGIN {push @INC, $ENV{GENESIS_LIB} ? $ENV{GENESIS_LIB} : $ENV{HOME}.'/.genesis/
 
 use parent qw(Genesis::Hook);
 
-use Genesis qw/bail info run warning/;
+use Genesis qw/bail info run warning error/;
 
 # init - Initialize the hook {{{
 sub init {
