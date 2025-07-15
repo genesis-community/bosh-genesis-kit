9d95b45fef3b77e1ced11cae5bb50a698c1f4c64
Wayne E. Seguin <wayneeseguin@gmail.com>
2025-07-15 10:13:12 -0400

    stackint CC m1a.2d and m1a.4d

--- hooks/cloud-config.pm
+++ hooks/cloud-config.pm
@@ -100,9 +100,9 @@ sub perform {
 					},
 					stackit => {
 						'instance_type' => $self->for_scale({
-							dev => 'm1.2',
-							prod => 'm1.3'
-						}, 'm1.2'),
+							dev => 'm1a.2d',
+							prod => 'm1a.4d'
+						}, 'm1a.2d'),
 						'boot_from_volume' => $self->TRUE,
 						'root_disk' => {
 							'size' => 32 # in gigabytes
